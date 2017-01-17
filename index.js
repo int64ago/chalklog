38bdf1e97d7128a61c9e8295783b8b021dcbf245
Cody Chan <int64ago@gmail.com>
2017-01-17 11:30:38 +0800

    style: untablify all

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function ChalkLog() {
 };
 
 var styles = (function () {
-	var ret = {};
-	Object.keys(chalk.styles).forEach(function (key) {
-		ret[key] = {
-			get: function () {
-				return build.call(this, this._styles.concat(key));
-			}
-		};
-	});
-	return ret;
+  var ret = {};
+  Object.keys(chalk.styles).forEach(function (key) {
+    ret[key] = {
+      get: function () {
+        return build.call(this, this._styles.concat(key));
+      }
+    };
+  });
+  return ret;
 })();
 
 var proto = defineProps(function() {}, styles);
@@ -26,24 +26,24 @@ var build = function(_styles) {
     builder._styles.forEach(function(_style) {
       fn = fn[_style];
     });
-		return log(fn.apply(builder, arguments));
-	};
-	builder._styles = _styles;
-	builder.enabled = this.enabled;
-	builder.__proto__ = proto;
-	return builder;
+    return log(fn.apply(builder, arguments));
+  };
+  builder._styles = _styles;
+  builder.enabled = this.enabled;
+  builder.__proto__ = proto;
+  return builder;
 };
 
 function init() {
-	var ret = {};
-	Object.keys(styles).forEach(function (name) {
-		ret[name] = {
-			get: function () {
-				return build.call(this, [name]);
-			}
-		};
-	});
-	return ret;
+  var ret = {};
+  Object.keys(styles).forEach(function (name) {
+    ret[name] = {
+      get: function () {
+        return build.call(this, [name]);
+      }
+    };
+  });
+  return ret;
 }
 
 defineProps(ChalkLog.prototype, init());
